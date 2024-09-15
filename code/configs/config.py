14b5273773c4560ae47c57bcb737720188a402bd
HY-Wong <b03703026@ntu.edu.tw>
2024-09-15 22:02:21 +0200

    ITIN-CLIP & ITIN-CLIP-GDINO configs

---
+++ code/configs/config.py
@@ -0,0 +1,19 @@
+import yaml
+
+from typing import Dict
+
+
+class Config:
+	def __init__(self, config: Dict):
+		for key, value in config.items():
+			if isinstance(value, dict):
+				value = Config(value)
+			setattr(self, key, value)
+
+
+def load_config(config_path: str):
+	with open(config_path, 'r') as file:
+		config = yaml.safe_load(file)
+
+	config = Config(config)
+	return config
